526eb2e85aca8b62afbdf8534d08567c84fdb296
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-16 20:09:09 -0700

    Reverted

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -2,10 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 # enable debugging
-import cgitb, socket
+import cgitb
 cgitb.enable()
-hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname) 
 
 print "Content-Type: text/plain;charset=utf-8"
 print
@@ -13,5 +11,4 @@ print
 print "Hello World!"
 print '<html><body>'
 print 'Server time is', time.asctime(time.localtime())
-print 'Host IP is', IPAddr
 print '</body></html>'
