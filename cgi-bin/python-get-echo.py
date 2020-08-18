4d4ce4dbf7782f2d601f1bc8d10999faf0612c5d
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 11:43:21 -0700

    FIXING JSON

--- cgi-bin/python-get-echo.py
+++ cgi-bin/python-get-echo.py
@@ -2,8 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 # enable debugging
+from urlparse import urlparse
 import cgitb, time, os
 cgitb.enable()
+
+st = urlparse.parse_qs(os.environ['QUERY_STRING'])
 
 print "Content-Type: text/html;charset=utf-8"
 print
@@ -11,4 +14,9 @@ print '<html><head><title>GET Request Echo</title> \
 </head><body><h1 align="center">Get Request Echo</h1> \
 <hr>'
 print '<b>Query String:</b>', os.environ['QUERY_STRING']
+if(len(os.environ['QUERY_STRING']) = 0):
+  st = urlparse.parse_qs(os.environ['QUERY_STRING'])
+  for key, value in st.items():
+    print key, ':', value
+
 print '</body></html>'
