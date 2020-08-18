c5fe240909927f70bf24213f05357ffbca28789f
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 16:37:08 -0700

    PHP-ENV

--- cgi-bin/php-env.php
+++ cgi-bin/php-env.php
@@ -5,7 +5,38 @@
 <h1>Team PJP - ENV Variables</h1>
 
 <?php
-print_r($GLOBALS)
+foreach($_SERVER as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_REQUEST as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_POST as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_GET as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_FILES as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_ENV as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_COOKIE as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
+foreach($_SESSION as $key => $value) {
+  echo $key . " = " . $value;
+  echo "<br>";
+}
 ?>
 
 </body>
