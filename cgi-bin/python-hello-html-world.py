47d997cddd71cb9d52c2f2468e6b730f85783a0f
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 02:02:10 -0700

    Fixed IP for address

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -2,10 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 # enable debugging
-import cgitb, time, socket
+import cgitb, time, os
 cgitb.enable()
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
 print "Content-Type: text/html;charset=utf-8"
 print
@@ -18,6 +16,6 @@ print '</head>'
 print '<body><p>'
 print 'Server time is', time.asctime(time.localtime())
 print '</p><p>'
-print 'User IP is', ip_address
+print 'User IP is', os.environ[REMOTE_ADDR]
 print '</p></body>'
 print '</html>'
