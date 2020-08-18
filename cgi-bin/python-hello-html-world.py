542186c4435c7599437debaf43deb1a750367617
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 01:53:29 -0700

    Added to Hellow World

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -2,18 +2,22 @@
 # -*- coding: UTF-8 -*-
 
 # enable debugging
-import cgitb, time
+import cgitb, time, socket
 cgitb.enable()
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
 print "Content-Type: text/html;charset=utf-8"
 print
 print '<html>'
 print '<head>'
-print '<h1>'
+print '<h1 align="center">'
 print "Hello World!"
 print '</h1>'
 print '</head>'
-print '<body>'
+print '<body><p>'
 print 'Server time is', time.asctime(time.localtime())
-print '</body>'
+print '</p><p>'
+print 'User IP is', ip_address
+print '</p></body>'
 print '</html>'
