f69f8e87de9c99b747e8a64459778c0a255341f2
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 16:43:25 -0700

    PHP-POST AND GET

---
+++ cgi-bin/php-get-echo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Team PJP - GET ECHO</h1>
+
+<?php
+echo "Query String = " . getenv('QUERY_STRING');
+?>
+
+</body>
+</html>
---
+++ cgi-bin/php-post-echo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Team PJP - POST ECHO</h1>
+
+<?php
+echo "Message Body = " . stream_get_contents(STDIN);
+?>
+
+</body>
+</html>
