fd08e5ed08b191f33f0a6b2f6cb3aa7f3e763f40
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 03:22:25 -0700

    Added Google analytics script

---
+++ cgi-bin/python-get-echo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb, time, os
+cgitb.enable()
+
+print "Content-Type: text/html;charset=utf-8"
+print
+print '<!DOCTYPE html><html><head><title>GET Request Echo</title> \
+</head><body><h1 align="center">Get Request Echo</h1> \
+<hr>'
+print '<b>Query String:</b>'
