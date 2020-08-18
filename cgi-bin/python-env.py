e4482b39b6a99802d60da406a6689215a64e40e1
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 02:43:41 -0700

    Added New Lines for Env

--- cgi-bin/python-env.py
+++ cgi-bin/python-env.py
@@ -11,5 +11,5 @@ print '<html><head><title>Environment Variables</title></head> \
       <body><h1 align=center>Environment Variables</h1> \
       <hr/>\n'
 for param in os.environ.keys():
-   print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+   print "<b>%20s</b>: %s<\br>\n" % (param, os.environ[param])
 print '</body></html>'
