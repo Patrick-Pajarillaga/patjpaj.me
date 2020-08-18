c48902b6305c4b3f2b90f420e7ae7088a9fa40dd
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 14:52:15 -0700

    AHAHHAHAH

--- cgi-bin/python-state-demo2.py
+++ cgi-bin/python-state-demo2.py
@@ -12,8 +12,8 @@ cookie.load(cookie_string)
 print "Content-Type: text/html;charset=utf-8"
 print
 print '<html><head><title>Python Session 2</title> \
-</head><body><h1 align="center">Python Session</h1> \
+</head><body><h1 align="center">Python Session 2</h1> \
 <hr>'
 print 'Name:', cookie['username'].value, '<br>'
-print '<a href="/cgi-bin/python-state-demo1.py">Session 1</a>'
+print '<a href="/python-cgiform.html">Session 1</a>'
 print '</body></html>'
