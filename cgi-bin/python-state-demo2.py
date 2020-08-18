a244e6ac8b1241b032ed8dbdabf9bbd0e7d2289d
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-18 13:57:15 -0700

    Session

--- cgi-bin/python-state-demo2.py
+++ cgi-bin/python-state-demo2.py
@@ -8,14 +8,13 @@ cgitb.enable()
 form = cgi.FieldStorage()
 
 cookie = Cookie.SimpleCookie()
-name = form.getvalue('username')
-cookie['username'] = name
+name = cookie['username']
 
 print "Content-Type: text/html;charset=utf-8"
 print
-print '<html><head><title>Python Session 1</title> \
+print '<html><head><title>Python Session 2</title> \
 </head><body><h1 align="center">Python Session</h1> \
 <hr>'
 print 'Name:', name, '<br>'
-print '<a href="./python-state-demo2.py">Session 2</a>'
+print '<a href="./python-state-demo1.py">Session 1</a>'
 print '</body></html>'
