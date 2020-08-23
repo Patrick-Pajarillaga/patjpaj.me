da67b113dc74dd385320ea5704e416911964cb65
Patrick-Pajarillaga <Patrickpajarillaga22@yahoo.com>
2020-08-23 01:41:21 -0700

    REEEE

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-var express = require('express');
+// app.js file
+
 var jsonServer = require('json-server');
 
-var server = express();
-// ...
+// Returns an Express server
+var server = jsonServer.create();
 
-// You may want to mount JSON Server on a specific end-point, for example /api
-// Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults()); 
-server.use('/api', jsonServer.router('db.json'));
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+
+// Add custom routes
+// server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
+
+// Returns an Express router
+var router = jsonServer.router('db.json');
+
+server.use(router);
 
 server.listen(3000);
